65c6ed06dedfbeea172d45c2e6ddde1805cf7503
natbutter <natbutter@gmail.com>
2021-08-27 15:07:50 +1000

    removed experimental multiprocessing

--- extract_point_from_raster_buffer.py
+++ extract_point_from_raster_buffer.py
@@ -12,15 +12,23 @@ import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
 
-from numba import jit
-
-#import swifter
-
 #Python base libraries
 import re
 import glob
 import multiprocessing as mp
 import time
+
+import numba
+
+#JIT directives applied to functions apply numbas'
+#llvm compiler. That should speed stuff up.
+from numba import jit
+
+import swifter
+from numba import cuda
+
+import dask.dataframe as dd
+from dask.multiprocessing import get
 
 #conda install -c conda-forge gdal
 #pip install arcgis
@@ -28,115 +36,83 @@ import time
 
 
 
-#RASTERIO
-# tic=time.time()
-# src = rasterio.open(folder+tiffile)
-# array_rasterio = src.read(1)
-# nodataval= src.nodatavals
-
-# if np.any(array_rasterio == nodataval):
-    # array_rasterio[array_rasterio == nodataval] = np.nan
-    
-# print("Read Raster with Rasterio", time.time() - tic)
-# print(np.shape(array_rasterio))
+#### Functions #####
+## Will eventually move these to generic file for use by all
+## extract tools.
+####
 
 
-
+@jit(nopython=True)
 def get_coords_at_point(gt, lon, lat):
-    row = int((lon - gt[0])/gt[1])
-    col = int((lat - gt[3])/gt[5])
-    return (col, row)
+	'''
+	Given a point in some coordinate reference (e.g. lat/lon)
+	Find the closest point to that in an array (e.g. a raster)
+	and return the index location of that point in the raster.
+	
+	INPUTS
+	gt: output from "gdal_data.GetGeoTransform()"
+	lon: x/row-coordinate of interest
+	lat: y/column-coordinate of interest
+
+	RETURNS
+	col: y index value from the raster
+	row: x index value from the raster
+	'''
+
+	row = int((lon - gt[0])/gt[1])
+	col = int((lat - gt[3])/gt[5])
+	return (col, row)
 
 
-
-
-
-##############
 @jit(nopython=True)
 def points_in_circle(circle, arr):
-    '''
-    A generator to return all points whose indices are within given circle.
-    http://stackoverflow.com/a/2774284
-    Warning: If a point is near the the edges of the raster it will not loop 
-    around to the other side of the raster!
-    '''
-    i0,j0,r = circle
-    
-    def intceil(x):
-        return int(np.ceil(x))  
+	'''
+	A generator to return all points whose indices are within a given circle.
+	http://stackoverflow.com/a/2774284
+	Warning: If a point is near the the edges of the raster it will not loop 
+	around to the other side of the raster!
+	'''
+	i0,j0,r = circle
+	
+	def intceil(x):
+		return int(np.ceil(x))  
 
-    for i in range(intceil(i0-r),intceil(i0+r)):
-        ri = np.sqrt(r**2-(i-i0)**2)
-        for j in range(intceil(j0-ri),intceil(j0+ri)):
-            if (i >= 0 and i < len(arr[:,0])) and (j>=0 and j < len(arr[0,:])):               
-                yield arr[i][j]
-
-#            
-                                          
+	for i in range(intceil(i0-r),intceil(i0+r)):
+		ri = np.sqrt(r**2-(i-i0)**2)
+		for j in range(intceil(j0-ri),intceil(j0+ri)):
+			if (i >= 0 and i < len(arr[:,0])) and (j>=0 and j < len(arr[0,:])):               
+				yield arr[i][j]        
+										  
 
 #
-def coregRaster(j0,i0,data,region):
-    '''
-    Finds the mean value of a raster, around a point with a specified radius.
-    point - array([longitude,latitude])
-    data - array
-    region - integer, same units as data
-    '''
-    pts_iterator = points_in_circle((i0,j0,region), data)
-    pts = np.array(list(pts_iterator))
+def coregRaster(i0,j0,data,region):
+	'''
+	Coregisters a point with a buffer region of a raster. 
+
+	INPUTS
+	i0: x/row-index of point of interest
+	j0: y/column-index of point of interest
+	data: two-dimensional numpy array (raster)
+	region: integer, same units as data resolution
+
+	RETURNS
+	pts: all values from array within region
+
+
+	'''
+	pts_iterator = points_in_circle((i0,j0,region), data)
+	pts = np.array(list(pts_iterator))
 
 	#Count area that contributed to calc
-    squares= np.count_nonzero(~np.isnan(pts))
+	squares= np.count_nonzero(~np.isnan(pts))
 
-    return(np.nansum(pts),squares)
-
-
-
-def neighbour_distance(gdf_chunk):
-    #pop=gdf_chunk.mean()
-	#pop=gdf_chunk.apply(lambda x: coregRaster(x[0], x[1],array_gdal,7), axis=1)
-	pop = np.array(map(coregRaster, x[0], x[1],array_gdal,7))
-	return(pop)
-
-#def neighbour_distance1(gdf_chunk):
-#    return(gdf_chunk.mean())
+	return(np.nansum(pts),squares)
 
 
 #####################
 
-
-#######
-def parallelize():
-	tic=time.time()
-	cpus = mp.cpu_count()
-	pool = mp.Pool(processes=cpus)
-	print("Opened pool", time.time() - tic)
-	print(pool)
-
-	intersection_chunks = np.array_split(sjer_plots_points.ind, cpus)
-
-	print(intersection_chunks[0])
-	print(np.shape(pd.DataFrame(intersection_chunks[0])))
-
-	tic=time.time()
-	#[print(chunk) for chunk in intersection_chunks]
-	print("running parallel...")
-	#intersection_results = pd.concat(pool.map(neighbour_distance, ([0,1],[1,2],[3,4])))
-	chunk_processes = [pool.apply_async(coregRaster, args=(x[0], x[1],array_gdal,7)) for chunk in intersection_chunks]
-	print("Apply-ed Pool", time.time() - tic)
-
-	pool.close()
-	pool.join()
-
-	#intersection_results = [chunk.get() for chunk in chunk_processes]
-
-	print(np.shape(intersection_results))
-
-	return(intersection_results)
-
-
 if __name__ == '__main__':
-    
+	
 	print("Running...")
 	tic=time.time()
 	pointsfile="AUS_points_5km.shp"
@@ -164,11 +140,13 @@ if __name__ == '__main__':
 	tic=time.time()
 	sjer_plots_points["ind"] = sjer_plots_points.apply(lambda x: get_coords_at_point(gt, x.geometry.x, x.geometry.y), axis=1)
 	print("Assigned index locations to points", time.time() - tic)
-	##Try square overlaps instead of circles!!!
+	
 
 
 	tic=time.time()
-	pop_area = sjer_plots_points.apply(lambda x: coregRaster(x.ind[0], x.ind[1],array_gdal,7), axis=1)
+	for row in sjer_plots_points.itertuples():
+		pop_area = coregRaster(row.ind[0], row.ind[1],array_gdal,7)
+	#pop_area = sjer_plots_points.apply(lambda x: coregRaster(x.ind[0], x.ind[1],array_gdal,7), axis=1)
 	print("Coregister raster, determine pop and area", time.time() - tic)
 	print(np.shape(pop_area))
 
