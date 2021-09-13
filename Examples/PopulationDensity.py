0355a7a3f633f2beba1df997e20f79a925e422d9
natbutter <natbutter@gmail.com>
2021-09-13 18:30:26 +1000

    broken kdtree

--- Examples/PopulationDensity.py
+++ Examples/PopulationDensity.py
@@ -13,6 +13,7 @@ import time
 #import multiprocessing
 #pool = multiprocessing.Pool(processes=2)
 
+import scipy
 from scipy.signal import convolve2d
 
 from scipy.ndimage import convolve
@@ -67,7 +68,8 @@ def get_coords_at_point(gt, lon, lat):
 	col: y index value from the raster
 	row: x index value from the raster
 	'''
-
+	# print(lon, gt[0], gt[1])
+	# print(lat, gt[3], gt[5])
 	row = int((lon - gt[0])/gt[1])
 	col = int((lat - gt[3])/gt[5])
 	return (col, row)
@@ -125,9 +127,16 @@ def coregRaster(i0,j0,data,region):
 #def convertKM2units(gt):
 		#figure out conversion from km to grid units
 
+def cartesian_product(*arrays):
+    la = len(arrays)
+    dtype = np.result_type(*arrays)
+    arr = np.empty([len(a) for a in arrays] + [la], dtype=dtype)
+    for i, a in enumerate(np.ix_(*arrays)):
+        arr[...,i] = a
+    return arr.reshape(-1, la)
 
 @delayed
-def poprast_prep(pth,grid,buffs):
+def poprast_prep(pth,grid,buffs,g):
 
 	print("Running on", pth) 
 
@@ -140,18 +149,66 @@ def poprast_prep(pth,grid,buffs):
 	if np.any(array_gdal == nodataval):
 		array_gdal[array_gdal == nodataval] = np.nan
 
+
+	###### this assumes each raster might be different 
+
 	t1=time.time()
-	print("Read",pth[-26:-20],",finding indexes...")
+
+	print("Shape of array_gdal:",np.shape(array_gdal))
+
+	startx=gt[0]
+	stepx=gt[1]
+	endx=startx+(np.shape(array_gdal)[1]+1)*stepx
+	xx = np.arange(startx,endx,stepx)
+	#xx=np.arange(startx,gt[0]+1000*7401,1000)
+	
+	starty=gt[3]
+	stepy=gt[5]
+	endy=starty+(np.shape(array_gdal)[0]+1)*stepy
+	yy = np.arange(endy,starty,-stepy)
+	print(xx,yy)
+	arr = cartesian_product(yy, xx)
+
+	print("Shape of index array:",np.shape(arr),np.shape(xx),np.shape(yy))
+	
+	print(arr)
+	print(g)
+	
+	tic=time.time()
+	tree = scipy.spatial.cKDTree(arr)
+	print("Made tree from gdal in",time.time()-tic)
+
+	tic=time.time()
+	_, indexes = tree.query(g)
+	print("Queried tree in",time.time()-tic)
+
+	tic=time.time()
+	arrind = list(zip(*np.unravel_index(indexes,np.shape(array_gdal))))
+	dfind=pd.Series(arrind)
+	grid['ind'] = dfind 
+	print("Zipped index index in",time.time()-tic)
+	
+	print("Indexed points loop", pth[-26:-20], "Time:",time.time()-t1)
+
+	#print(dfind)
+	print(grid["ind"])
+
+	#######33
+	# t1=time.time()
+	# print("Read",pth[-26:-20],",finding indexes...")
 	ind=[]
 	for row in grid.itertuples():
+		print(row.geometry.x, row.geometry.y)
 		ind.append(get_coords_at_point(gt, row.geometry.x, row.geometry.y))
 
 	grid["ind"]=ind
-	print("Indexed points loop", pth[-26:-20], "Time:",time.time()-t1)
+	print(grid["ind"])
+	# print("Indexed points loop", pth[-26:-20], "Time:",time.time()-t1)
 
 	#loop is 10x faster than apply
 	#t1=time.time()
 	#grid["ind"] = grid.apply(lambda x: get_coords_at_point(gt, x.geometry.x, x.geometry.y), axis=1)
+	#print(grid["ind"])
 	#print("Indexed points", pth, "Time:",time.time()-t1)
 
 	poplist = []
@@ -159,17 +216,16 @@ def poprast_prep(pth,grid,buffs):
 		#Set buffer to index units
 		b=np.ceil(buff/gt[1])
 		t1=time.time()
-		#Create the 
+		#Run the convolution with the buffer
 		y2_large = buffer_convolve2d(array_gdal,b)
-		#[print(x) for x in grid["ind"]]
-		#[print(y2_large[x]) for x in grid["ind"]]
+		#Find the value of the convolved array at each point of interest
 		pop = [y2_large[x] for x in grid["ind"]]
 		poplist.append(pop)
-		#print(pop)
+
 		print("Done pop", buff,b, pth[-26:-20], "Time:",time.time()-t1)
 
 	yr = str(20) + re.sub(".*(apg|APG)(\\d{2}).*", "\\2", pth)
-
+	print(poplist)
 	popdf = gpd.GeoDataFrame(poplist, index = ["popdens" + str(b) for b in buffs]).T
 	popdf.insert(0, 'FID', grid.FID)
 	popdf.insert(0, 'year', yr)
@@ -178,67 +234,30 @@ def poprast_prep(pth,grid,buffs):
 
 	return(popdf)
 
-
-@delayed
-def poprast_prepOLD(pth,grid,buffs):
-	print("Running on", pth) 
-
-	#Open the raster file and set it up
-	gdal_data = gdal.Open(pth)
-	gdal_band = gdal_data.GetRasterBand(1)
-	nodataval = gdal_band.GetNoDataValue()
-	array_gdal = gdal_data.ReadAsArray().astype(np.float)
-	gt = gdal_data.GetGeoTransform()
-	if np.any(array_gdal == nodataval):
-		array_gdal[array_gdal == nodataval] = np.nan
-
-	#Create an empty array to store the sampled points stats in.
-	pop=np.empty(len(grid))
-	pop[:] = np.NaN
-
-	poplist=[]
-	for buff in buffs:
-		#Set the buffer size (in m) to index units
-		print("Registering",pth[-26:-20],buff)
-		b=np.ceil(buff/gt[1])
-
-		tic=time.time()
-		for i,row in enumerate(grid.itertuples()):
-			ind=get_coords_at_point(gt, row.geometry.x, row.geometry.y)
-			pop[i]=coregRaster(ind[0], ind[1], array_gdal,b)
-		
-		#print(pop.tolist())
-		poplist.append(pop.tolist())
-		print("Done pop",pth[-26:-20],buff,time.time()-tic)
-	
-	yr = str(20) + re.sub(".*(apg|APG)(\\d{2}).*", "\\2", pth)
-
-	popdf = gpd.GeoDataFrame(poplist, index = ["popdens" + str(b) for b in buffs]).T
-	popdf.insert(0, 'FID', grid.FID)
-	popdf.insert(0, 'year', yr)
-
-	#Close gdal file
-	gdal_data=None
-
-	return(popdf)
 
 ############
 
 if __name__ == "__main__":
 
 	## Read in population rasters
-	poprasts = glob.glob('ABS1x1km_Aus_Pop_Grid_2006_2020/' +
-                     'data_provided/*.tif')
+	#poprasts = glob.glob('ABS1x1km_Aus_Pop_Grid_2006_2020/' +
+    #                 'data_provided/*.tif')
 
-	#poprasts=["ABS1x1km_Aus_Pop_Grid_2006_2020/data_provided/apg06e_f_001_20210512.tif",
-	#			"ABS1x1km_Aus_Pop_Grid_2006_2020/data_provided/apg09e_f_001_20210512.tif"]
+	poprasts=["ABS1x1km_Aus_Pop_Grid_2006_2020/data_provided/apg06e_f_001_20210512.tif"]#,
+				#"ABS1x1km_Aus_Pop_Grid_2006_2020/data_provided/apg09e_f_001_20210512.tif"]
 
-	buffs = [700, 1000, 1500, 2000, 3000, 5000, 10000]
+	buffs = [700, 10000] #, 1500, 2000, 3000, 5000, 10000]
 
 	tic=time.time()
 	print("Reading points shapefile...")
-	grid = gpd.read_file('AUS_points_1km.shp')
-	#grid = gpd.read_file('point03.shp')
+	#grid = gpd.read_file('AUS_points_5km.shp')
+	grid = gpd.read_file('point06.shp')
+
+	tic=time.time()
+	g = np.column_stack((grid.geometry.x.to_list(),grid.geometry.y.to_list()))
+	print("Stacked points in",time.time()-tic)
+	
+	
 	## Add an FID in there - if one doesn't already exist
 	grid.insert(0, 'FID', range(1, len(grid) + 1))
 
@@ -246,7 +265,7 @@ if __name__ == "__main__":
 
 	t = []
 	for pth in poprasts:
-		t.append(poprast_prep(pth,grid,buffs))
+		t.append(poprast_prep(pth,grid,buffs,g))
 		#dd=poprast_prep(pth,grid,buffs)
 
 	#client = Client()
