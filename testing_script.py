cc748f5ed907624c79ca679e95be72f9a99826c3
Henry Lydecker <henry.lydecker@sydney.edu.au>
2021-10-20 14:01:11 +1100

    Update testing_script.py

--- testing_script.py
+++ testing_script.py
@@ -12,7 +12,7 @@ import rasterstats as rs
 """Arguments"""
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-f","--file", default = "./data/layers/ABS1x1km_Aus_Pop_Grid_2006_2020/data_provided/*.tif", type=Path,help="One or more raster layers selected for data extraction.")
+ap.add_argument("-f","--file", default = "./data/layers/Land_Cover_MODIS/tree_and_water_coverage_2017_2018/data_provided/tree_cover_2018_20210510.tif", type=Path,help="One or more raster layers selected for data extraction.")
 ap.add_argument("-g","--grid", default = "./data/grids/testing_points.rds", type=Path, help="Dataframe with FID and X Y locations to extract.")
 ap.add_argument("-o","--output", default = "./output", type=Path)
 args = ap.parse_args()
@@ -20,23 +20,22 @@ args = ap.parse_args()
 def open_gdal(filename):
 	#Open a raster file using gdal and set it up
 	gdal_data = gdal.Open(filename)
-	# gdal_band = gdal_data.GetRasterBand(1)
-	# nodataval = gdal_band.GetNoDataValue()
+	gdal_band = gdal_data.GetRasterBand(1)
+	nodataval = gdal_band.GetNoDataValue()
 	array_gdal = gdal_data.ReadAsArray().astype(np.float)
-	# gt = gdal_data.GetGeoTransform()
-	# wkt = gdal_data.GetProjection()
-	# if np.any(array_gdal == nodataval):
-	# 	array_gdal[array_gdal == nodataval] = np.nan
+	gt = gdal_data.GetGeoTransform()
+	wkt = gdal_data.GetProjection()
+	if np.any(array_gdal == nodataval):
+		array_gdal[array_gdal == nodataval] = np.nan
 
-	# gdal_data.FlushCache()
-	# gdal_data = None
-	# del gdal_data
+	gdal_data.FlushCache()
+	gdal_data = None
+	del gdal_data
+
+
 
 	#Return the raster array and the spatial details about the raster
-	return(array_gdal)
-
-# test = open_gdal(str(args.file))
-# print(test)
+	return(array_gdal,gt,wkt,gdal_band,nodataval)
 
 print("Reading points rds file...")
 grid = pyreadr.read_r(str(args.grid))
