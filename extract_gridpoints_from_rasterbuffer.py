7cf24668e95a26c700b5382cf91226ba5007e470
Seb Haan <sebhaan@users.noreply.github.com>
2021-11-10 14:21:15 +1100

    Fix nodata values in final image

--- extract_gridpoints_from_rasterbuffer.py
+++ extract_gridpoints_from_rasterbuffer.py
@@ -11,19 +11,22 @@ Arguments:
 -g or --grid: path+filename of destination grid raster
 -o or --output: output pathname
 
+how to run:
+python3 -f INPUTFILENAME -g DESTINATIONFILENAME -o OUTPATHNAME extract_gridpoints_from_rasterbuffer.py
+
 
 Notes: 
-- buffers smaller than input raster resolution are not buffered, only reprojected to destintaion grid coordinates 
+- buffers smaller than input raster resolution are not buffered, only reprojected to destination grid coordinates 
 (to disable this function set buffer_min = 0)
-- buffers ar currently hardcoded at buffers = [700, 1000, 1500, 2000, 3000, 5000, 10000]
+- buffers are currently hard-coded at buffers = [700, 1000, 1500, 2000, 3000, 5000, 10000]
 (Change in main function if needed)
-- resampling with nearest neighbor is fastest method but on can also use linear interpolation 
+- resampling with nearest neighbour is fastest method but on can also use linear interpolation 
 (for change replace in code  resampling=Resampling.nearest with other method)
 
 
 Possible improvements:
 
-- include automatic crs check and transfromation to destination grid crs (now taken into account by fitting transformation with rasterio)
+- include automatic crs check and transformation to destination grid crs (now taken into account by fitting transformation with rasterio)
 - automated test of extracted stats for some test points
 - save transformation matrix for reprojection once and reuse for each buffer (--> can speed up computation)
 
@@ -83,7 +86,7 @@ ap = argparse.ArgumentParser()
 # ap.add_argument("-f","--file", default = "./data/ABS1x1km_Aus_Pop_Grid_2006_2020/data_provided/*.tif", type=Path)
 # ap.add_argument("-g","--grid", default = "./data/AUS_points_5km.rds", type=Path)
 # ap.add_argument("-o","--output", default = "./output", type=Path)
-ap.add_argument("-f","--file", default = "./data/apg18e_1_0_0_20210512_crs3577.tif", type=Path)
+ap.add_argument("-f","--file", default = "./data/apg18e_1_0_0_20210512.tif", type=Path)
 ap.add_argument("-g","--grid", default = "./data/grid_to_do_APMMA_NSW_20211018.tif", type=Path)
 ap.add_argument("-o","--output", default = "./output", type=Path)
 args = ap.parse_args()
@@ -244,14 +247,12 @@ if __name__ == "__main__":
 			# mask image
 			with rasterio.open(fname_warp_temp) as src:
 				data_new = src.read()[0]
+				#nodata = -3.4e38
 				data_new[grid == grid_nodata] = grid_nodata
-				#data_new[np.round(grid,1) == np.round(grid_nodata,1)] = grid_nodata
-				#data_new[np.round(data_new,1) == np.round(grid_nodata,1)] = grid_nodata
-				#data_new[np.round(data_new,1) == np.round(grid_nodata,1)] = grid_nodata
 				#data[data < grid_nodata] = grid_nodata
 				#data[~np.isfinite(data)] = grid_nodata
 				kwargs = src.meta.copy()
-				kwargs.update({'src_nodata': grid_nodata,'dst_nodata': grid_nodata})
+				kwargs.update({'nodata': grid_nodata})
 				with rasterio.open(fname_final_out,'w', **kwargs) as dest_file:
 					dest_file.write(data_new, 1)
 			src.close()
